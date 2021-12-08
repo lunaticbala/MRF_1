f235a354c5ecd24f2ecb9291df3c4597661b1638
lunaticbala <87910861+lunaticbala@users.noreply.github.com>
2021-12-08 09:45:39 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,65 @@
+const scores = [
+    {
+      marks: 32,
+      name: "Yvette Merritt"
+    },
+    {
+      marks: 57,
+      name: "Lillian Ellis"
+    },
+    {
+      marks: 22,
+      name: "Mccall Carter"
+    },
+    {
+      marks: 21,
+      name: "Pate Collier"
+    },
+    {
+      marks: 91,
+      name: "Debra Beard"
+    },
+    {
+      marks: 75,
+      name: "Nettie Hancock"
+    },
+    {
+      marks: 20,
+      name: "Hatfield Hodge"
+    }
+  ];
+
+// task: 1  
+
+// const d=scores.map(item=>item.name);
+// console.log(d);
+
+// task : 2
+
+// const d=scores.filter(item=>item.marks>=40);
+// console.log(d);
+
+// task : 3
+
+// const d=scores.filter(item=>item.marks<=40);
+// const f=d.map(item=>item.name);
+// console.log(f);
+
+
+// task : 4
+
+// const d=scores.reduce((item,element)=>{
+//     return item+element.marks;
+// },0)
+
+// const a=(d/scores.length);
+// console.log(a);
+
+// task : 5
+
+// var max = 
+// scores.reduce((acc, shot) => acc = acc > shot.marks ? acc : shot.marks, 0);
+
+// console.log(max);
+// const t=max.map(item=>item.name);
+// console.log(t);
